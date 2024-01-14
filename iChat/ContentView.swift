458bd44f6e221c3e18e87d5e9e329d9bdbb4ffa2
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2024-01-14 23:57:28 +0530

    Initial Commit

---
+++ iChat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iChat
+//
+//  Created by Diptayan Jash on 14/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iChat/iChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  iChatApp.swift
+//  iChat
+//
+//  Created by Diptayan Jash on 14/01/24.
+//
+
+import SwiftUI
+
+@main
+struct iChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
