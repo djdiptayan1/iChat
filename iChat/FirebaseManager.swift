6a75a1a8284774f9ee533ddd30f694753e7a4fd3
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2024-01-15 15:01:18 +0530

    A separate file for handelling user data and firebase connection

--- iChat/FirebaseManager.swift
+++ iChat/FirebaseManager.swift
@@ -1,8 +1,55 @@
 import Firebase
-import SwiftUI
 import FirebaseStorage
+import SwiftUI
 
 import Firebase
+
+struct ChatUser {
+    let username, email, uid: String
+    let ProfilePic: String
+}
+
+class GetUserData: ObservableObject {
+    @Published var errorMessage = ""
+    @Published var username = ""
+    @Published var email = ""
+    @Published var uid = ""
+    @Published var ProfilePic = ""
+    
+    init() {
+        fetchCurrentUser()
+    }
+
+    private func fetchCurrentUser() {
+        guard let
+            email = FirebaseManager.shared.auth.currentUser?.email else {
+            errorMessage = "FAILED TO FETCH USER"
+            return
+        }
+        FirebaseManager.shared.firestore.collection("users")
+            .document(email).getDocument { snapshot, error in
+                if let error = error {
+                    self.errorMessage = "FAILED TO FETCH USER"
+                    print("FAILED TO FETCH USER", error)
+                    return
+                }
+                guard let data = snapshot?.data() else { return }
+                print(data)
+
+                let username = data["name"] as? String ?? ""
+                let email = data["email"] as? String ?? ""
+                let uid = data["uid"] as? String ?? ""
+                let ProfilePic = data["ProfilePic"] as? String ?? ""
+
+                let chatuser = ChatUser(username: username, email: email, uid: uid, ProfilePic: ProfilePic)
+
+                self.username = chatuser.username
+                self.email = chatuser.email
+                self.uid = chatuser.uid
+                self.ProfilePic = chatuser.ProfilePic
+            }
+    }
+}
 
 struct FirebaseUserManager {
     static func saveImageToFirebaseStorage(uid: String, image: UIImage, completion: @escaping (URL?, String?) -> Void) {
@@ -10,11 +57,11 @@ struct FirebaseUserManager {
             completion(nil, "Failed to convert image to data.")
             return
         }
-        
+
         let ref = FirebaseManager.shared.storage.reference(withPath: uid)
         let metadata = StorageMetadata()
         metadata.contentType = "image/jpeg"
-        
+
         ref.putData(imageData, metadata: metadata) { _, error in
             if let error = error {
                 completion(nil, "Failed to push image to Storage: \(error)")
@@ -29,7 +76,7 @@ struct FirebaseUserManager {
             }
         }
     }
-        
+
     static func storeUserInfo(email: String, uid: String, profilePicURL: URL, completion: @escaping (String?) -> Void) {
         let userData = ["email": email, "uid": uid, "ProfilePic": profilePicURL.absoluteString]
         FirebaseManager.shared.firestore.collection("users").document(uid).setData(userData) { error in
