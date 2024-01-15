85d968c0c35e040d76526ec3b28f30f9971236f6
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2024-01-15 12:56:25 +0530

    UI template

--- iChat/MainMessages/MainMessageView.swift
+++ iChat/MainMessages/MainMessageView.swift
@@ -1,8 +1,70 @@
-//
-//  MainMessageView.swift
-//  iChat
-//
-//  Created by Diptayan Jash on 15/01/24.
-//
+import SwiftUI
+struct MainMessageView: View {
+    
+    @State var ShowLogOutOptions = false
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                // Navbar
+                NavBar(ShowLogOutOptions: $ShowLogOutOptions)
+                ScrollView {
+                    ForEach(0 ..< 10, id: \.self) { _ in
+                        HStack(spacing: 16) {
+                            Image(systemName: "person.fill")
+                                .font(.system(size: 32))
+                                .accessibilityLabel("Profile Image")
+                                .padding(4)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 44)
+                                        .stroke(Color(.label), lineWidth: 1)
+                                )
 
-import Foundation
+                            VStack(alignment: .leading) {
+                                Text("username")
+                                    .font(.system(size: 14, weight: .bold))
+                                Text("msg sent")
+                                    .font(.system(size: 14))
+                                    .foregroundStyle(Color(.lightGray))
+                            }
+                            Spacer()
+                            Text("22d")
+                                .font(.system(size: 14, weight: .semibold))
+                        }
+                        Divider()
+                            .padding(.vertical, 8)
+                    }
+                }
+                .padding(.horizontal)
+            }
+            .overlay(
+                NewMsgButton(), alignment: .bottom)
+            .navigationBarHidden(true)
+        }
+    }
+}
+
+#Preview {
+    MainMessageView()
+}
+
+struct NewMsgButton: View {
+    var body: some View {
+        Button {
+        } label: {
+            HStack {
+                Spacer()
+                Text("New Message")
+                    .font(.system(size: 16, weight: .bold))
+                    .padding()
+                Spacer()
+            }
+            .foregroundStyle(.white)
+            .background(Color.blue)
+            .cornerRadius(32)
+            .padding(.horizontal)
+            .shadow(radius: 15)
+        }
+        
+    }
+}
